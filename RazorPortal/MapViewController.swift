85c491a4ac6788444a6fc64502cd2aaa2ed6cc39
Chris Drewry <chrisdrewry@Frankenstein.local>
2015-04-02 21:59:03 -0500

    Added dynamic pin generation based on user Added live GPS location of user on map

--- RazorPortal/MapViewController.swift
+++ RazorPortal/MapViewController.swift
@@ -5,13 +5,24 @@
 //  Created by Chris Drewry on 3/12/15.
 //  Copyright (c) 2015 Chris Drewry. All rights reserved.
 //
+// IMPORTANT SHIT:
+//      user: root
+//      pass: tu3xooGh
+//      when in doubt, var char[255]
+//
+//currently only pulls classes for apptest
 
 import Foundation
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var myMap: MKMapView!
+    
+    //globals
+    var data: NSArray = []
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +33,28 @@ class MapViewController: UIViewController {
         let region = MKCoordinateRegion(center: location, span: span)
         myMap.setRegion(region, animated: true)
         
+        //show current user location
+        locationManager.requestWhenInUseAuthorization()
+        self.myMap.mapType = MKMapType.Standard
+        self.myMap.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        //talk to database
+        getallrecords()
+        
+        
+        
         //Drops example pins
-        dropPin("Bell Engineering Center", subtitle: "BELL", lat: 36.067044, long: -94.171396)
-        dropPin("JB Hunt Center for Academic Excellence", subtitle: "JBHT", lat: 36.065958, long: -94.173698)
-        dropPin("Nanoscale Material Science and Engineering Building", subtitle: "NANO", lat: 36.066017, long: -94.169347)
-        dropPin("Science and Engineering", subtitle: "SCEN", lat: 36.066999, long: -94.172636)
-        dropPin("Arkansas Union", subtitle: "UNIN", lat: 36.068620, long: -94.175827)
-        dropPin("Vol Walker Hall", subtitle: "VOLW", lat: 36.068664, long: -94.172721)
-        dropPin("Mullins Library", subtitle: "MULL", lat: 36.068725, long: -94.173751)
+        //dropPin("Bell Engineering Center", subtitle: "BELL", lat: 36.067044, long: -94.171396)
+        //dropPin("JB Hunt Center for Academic Excellence", subtitle: "JBHT", lat: 36.065958, long: -94.173698)
+        //dropPin("Nanoscale Material Science and Engineering Building", subtitle: "NANO", lat: 36.066017, long: -94.169347)
+        //dropPin("Science and Engineering", subtitle: "SCEN", lat: 36.066999, long: -94.172636)
+        //dropPin("Arkansas Union", subtitle: "UNIN", lat: 36.068620, long: -94.175827)
+        //dropPin("Vol Walker Hall", subtitle: "VOLW", lat: 36.068664, long: -94.172721)
+        //dropPin("Mullins Library", subtitle: "MULL", lat: 36.068725, long: -94.173751)
     }
     
     override func didReceiveMemoryWarning() {
@@ -47,19 +72,38 @@ class MapViewController: UIViewController {
         myMap.addAnnotation(annotation)
     }
     
-    var data: NSArray = []
+    func dataOfJson(url: String) -> NSArray {
+        var data = NSData(contentsOfURL: NSURL(string: url)!)
+        return (NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as NSArray)
+    }
     
     //downloads user classes to an array
-    func getUserClasses() {
-        //database info
-        let url : NSURL = NSURL(string: "http://uaf59189.ddns.uark.edu/Login.php")!
-        let request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
-        request.HTTPMethod = "POST"
-        //request.HTTPBody = data.dataUsingEncoding(NSUTF8StringEncoding);
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) {
-            (response, data, error) in
-            var output = NSString(data: data, encoding: NSUTF8StringEncoding)
-            //var array = self.JSONParseArray(output)
+    func getallrecords(){
+        let url = NSURL(string: "http://uaf59189.ddns.uark.edu/service.php")
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            var d = NSString(data: data, encoding: NSUTF8StringEncoding)
+            var arr = d!.componentsSeparatedByString("<")
+            // spliting the incoming string from "<" operator because before that operator is our required data and storing in array
+            var dataweneed:NSString = arr[0] as NSString
+            // arr[0] is the data before "<" operator and arr[1] is actually no use for us
+            if let data = NSJSONSerialization.JSONObjectWithData(dataweneed.dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSArray
+            {
+                for dd in data{
+                    var username : String = dd["username"]! as String
+                    var classname : String = dd["classname"]! as String
+                    var days : String = dd["days"]! as String
+                    var time : String = dd["time"]! as String
+                    var building : String = dd["building"]! as String
+                    var room : String = dd["room"]! as String
+                    var classcode : String = dd["classcode"]! as String
+                    
+                    if (username == "apptest") {
+                        self.dropPin(building, subtitle: " ", lat: 36.065958, long: -94.171396)
+                    }
+                }
+            }
         }
+        
+        task.resume()
     }
 }
