ed31d39969b5044c61eefbc7fb0dc09e443629dd
Chris Drewry <chrisdrewry@Frankenstein.local>
2015-04-14 13:13:55 -0500

    Changed functions to work with the April 8 Swift update.

--- RazorPortal/MapViewController.swift
+++ RazorPortal/MapViewController.swift
@@ -43,18 +43,18 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
         
         //talk to database
-        getallrecords()
+        //getallrecords()
         
         
         
         //Drops example pins
-        //dropPin("Bell Engineering Center", subtitle: "BELL", lat: 36.067044, long: -94.171396)
-        //dropPin("JB Hunt Center for Academic Excellence", subtitle: "JBHT", lat: 36.065958, long: -94.173698)
-        //dropPin("Nanoscale Material Science and Engineering Building", subtitle: "NANO", lat: 36.066017, long: -94.169347)
-        //dropPin("Science and Engineering", subtitle: "SCEN", lat: 36.066999, long: -94.172636)
-        //dropPin("Arkansas Union", subtitle: "UNIN", lat: 36.068620, long: -94.175827)
-        //dropPin("Vol Walker Hall", subtitle: "VOLW", lat: 36.068664, long: -94.172721)
-        //dropPin("Mullins Library", subtitle: "MULL", lat: 36.068725, long: -94.173751)
+        dropPin("Bell Engineering Center", subtitle: "BELL", lat: 36.067044, long: -94.171396)
+        dropPin("JB Hunt Center for Academic Excellence", subtitle: "JBHT", lat: 36.065958, long: -94.173698)
+        dropPin("Nanoscale Material Science and Engineering Building", subtitle: "NANO", lat: 36.066017, long: -94.169347)
+        dropPin("Science and Engineering", subtitle: "SCEN", lat: 36.066999, long: -94.172636)
+        dropPin("Arkansas Union", subtitle: "UNIN", lat: 36.068620, long: -94.175827)
+        dropPin("Vol Walker Hall", subtitle: "VOLW", lat: 36.068664, long: -94.172721)
+        dropPin("Mullins Library", subtitle: "MULL", lat: 36.068725, long: -94.173751)
     }
     
     override func didReceiveMemoryWarning() {
@@ -66,7 +66,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     func dropPin(title: String, subtitle: String, lat: CLLocationDegrees, long: CLLocationDegrees) {
         let annotation = MKPointAnnotation()
         let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
-        annotation.setCoordinate(location)
+        annotation.coordinate = location
         annotation.title = title
         annotation.subtitle = subtitle
         myMap.addAnnotation(annotation)
@@ -74,7 +74,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     func dataOfJson(url: String) -> NSArray {
         var data = NSData(contentsOfURL: NSURL(string: url)!)
-        return (NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as NSArray)
+        return (NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as! NSArray)
     }
     
     //downloads user classes to an array
@@ -84,21 +84,21 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             var d = NSString(data: data, encoding: NSUTF8StringEncoding)
             var arr = d!.componentsSeparatedByString("<")
             // spliting the incoming string from "<" operator because before that operator is our required data and storing in array
-            var dataweneed:NSString = arr[0] as NSString
+            var dataweneed:NSString = arr[0]as! NSString
             // arr[0] is the data before "<" operator and arr[1] is actually no use for us
             if let data = NSJSONSerialization.JSONObjectWithData(dataweneed.dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSArray
             {
                 for dd in data{
-                    var username : String = dd["username"]! as String
-                    var classname : String = dd["classname"]! as String
-                    var days : String = dd["days"]! as String
-                    var time : String = dd["time"]! as String
-                    var building : String = dd["building"]! as String
-                    var room : String = dd["room"]! as String
-                    var classcode : String = dd["classcode"]! as String
+                    var username : String = dd["username"]! as! String
+                    var classname : String = dd["classname"]! as! String
+                    var days : String = dd["days"]!as! String
+                    var time : String = dd["time"]! as! String
+                    var building : String = dd["building"]! as! String
+                    var room : String = dd["room"]! as! String
+                    var classcode : String = dd["classcode"]! as! String
                     
                     if (username == "apptest") {
-                        self.dropPin(building, subtitle: " ", lat: 36.065958, long: -94.171396)
+                        //self.dropPin(building, subtitle: " ", lat: 36.065958, long: -94.171396)
                     }
                 }
             }
