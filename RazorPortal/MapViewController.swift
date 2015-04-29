6455fd78a185af4151c9ede04e520b432106ff5d
cgdrewry <chris.g.drewry@gmail.com>
2015-04-28 19:12:23 -0500

    Cleaned up networking code.

--- RazorPortal/MapViewController.swift
+++ RazorPortal/MapViewController.swift
@@ -17,21 +17,22 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     //globals
     var data: NSArray = []
     let locationManager = CLLocationManager()
+    var account = "apptest"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //sets centered location to UARK campus
-        let location = CLLocationCoordinate2D(latitude: 36.0687, longitude: -94.1760)
-        let span = MKCoordinateSpanMake(0.04, 0.04)
-        let region = MKCoordinateRegion(center: location, span: span)
-        myMap.setRegion(region, animated: true)
-        
-        //if location services enabled
+        //location services check
         if (CLLocationManager.locationServicesEnabled())
         {
             //asks user for tracking permission
             self.locationManager.requestWhenInUseAuthorization()
+            
+            //centers location to uark campus
+            let location = CLLocationCoordinate2D(latitude: 36.0687, longitude: -94.1760)
+            let span = MKCoordinateSpanMake(0.04, 0.04)
+            let region = MKCoordinateRegion(center: location, span: span)
+            myMap.setRegion(region, animated: true)
             
             //sets location info
             locationManager.delegate = self
@@ -42,8 +43,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         }
         
         //talk to database
-        dropHardCodedPins()
-        //getallrecords()
+        //dropHardCodedPins()
+        getuserrecords()
     }
     
     //location manager method
@@ -59,6 +60,12 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     //only call for demonstrations where an account isnt already setup
     func dropHardCodedPins() {
+        //sets centered location to UARK campus
+        //let location = CLLocationCoordinate2D(latitude: 36.0687, longitude: -94.1760)
+        //let span = MKCoordinateSpanMake(0.04, 0.04)
+        //let region = MKCoordinateRegion(center: location, span: span)
+        //myMap.setRegion(region, animated: true)
+        
         dropPin("Bell Engineering Center", subtitle: "BELL", lat: 36.067044, long: -94.171396)
         dropPin("JB Hunt Center for Academic Excellence", subtitle: "JBHT", lat: 36.065958, long: -94.173698)
         dropPin("Nanoscale Material Science and Engineering Building", subtitle: "NANO", lat: 36.066017, long: -94.169347)
@@ -90,14 +97,18 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     //downloads user classes to an array
-    func getallrecords() {
+    func getuserrecords() {
+        /* I know this looks like a code monkey vomited syntax everywhere, but here's the big idea:
+        NSString is basically a str or char[]
+        arr is the raw JSON fetched string
+        dataSrc is a pointer to arr[head] */
+        
         let url = NSURL(string: "http://uaf59189.ddns.uark.edu/service.php")
         let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
             var d = NSString(data: data, encoding: NSUTF8StringEncoding)
             var arr = d!.componentsSeparatedByString("<")
             var dataSrc:NSString = arr[0]as! NSString
-            if let data = NSJSONSerialization.JSONObjectWithData(dataSrc.dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSArray
-            {
+            if let data = NSJSONSerialization.JSONObjectWithData(dataSrc.dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSArray {
                 for dd in data {
                     var username : String = dd["username"]! as! String
                     var classname : String = dd["classname"]! as! String
@@ -107,26 +118,35 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
                     var room : String = dd["room"]! as! String
                     var classcode : String = dd["classcode"]! as! String
                     
-                    if (username == "apptest") {
-                        var sub = "subtitle"
-                        var lat = 0.0
-                        var lng = 0.0
-                        if (building == "JBHT") {
-                            sub = "JB Hunt Center for Academic Excellence"
-                            lat = 36.065958
-                            lng = -94.173698
-                        } else if (building == "MEEG") {
-                            sub = "Mechanical Engineering"
-                            lat = 36.066359
-                            lng = -94.172850
-                        } else if (building == "BELL") {
-                            sub = "Bell Engineering Center"
-                            lat = 36.067044
-                            lng = -94.171396
-                        }
-                        
-                        //actually drops pin
-                        self.dropPin(classname, subtitle: building, lat: lat, long: lng)
+                    if (username == self.account) {
+                        self.getBuildingData(building)
+                    }
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    //handles building data fetching
+    func getBuildingData(building: String)
+    {
+        let url = NSURL(string: "http://uaf59189.ddns.uark.edu/serviceBuildings.php")
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+            var d = NSString(data: data, encoding: NSUTF8StringEncoding)
+            var arr = d!.componentsSeparatedByString("<")
+            var dataSrc:NSString = arr[0]as! NSString
+            if let data = NSJSONSerialization.JSONObjectWithData(dataSrc.dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSArray {
+                for dd in data {
+                    var bCode : String = dd["Building_Code"]! as! String
+                    var bName : String = dd["Building_Name"]! as! String
+                    var bLat : String = dd["Latitude"]!as! String
+                    var bLong : String = dd["Longitude"]! as! String
+                    
+                    if (building == bCode) {
+                        let dubLat = (bLat as NSString).doubleValue
+                        let dubLong = (bLong as NSString).doubleValue
+                        self.dropPin(bName, subtitle: bCode, lat: dubLat, long: dubLong)
                     }
                 }
             }
